d341337a13ca77114581eac605b0b170ff1f21c6
tcb12 <tcb@g.clemson.edu>
2020-10-02 14:37:02 -0400

    Improved wait and mouse movement detection
    Improved wait and mouse movement detection

--- src/main/java/com/tcb12/application/Main.java
+++ src/main/java/com/tcb12/application/Main.java
@@ -1,45 +1,46 @@
 package com.tcb12.application;
 
 import java.awt.*;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.Random;
 
 public class Main {
     public static final int MAX_Y = 400;
     public static final int MAX_X = 400;
-    public static final Integer ONE_Minute = 60000;
+    public static final Integer One_Minute = 60000;
 
     public static void main(String[] args) throws AWTException {
+        System.out.println("Starting...");
+
         Robot robot = new Robot();
         Random random = new Random();
 
+        // Declare variables
         int last_x = 0;
-        Instant last_time = Instant.now();
+        int last_y = 0;
 
+        // Stay alive
         while (true) {
-            Instant start = Instant.now();
+            // Get mouse location
+            PointerInfo aPointInfo = MouseInfo.getPointerInfo();
+            Point aPoint = aPointInfo.getLocation();
+            int x = (int) aPoint.getX();
+            int y = (int) aPoint.getY();
 
+            // Check if the mouse has not moved
+            if (x == last_x && y == last_y) {
+                System.out.println("Movement not detected, moving mouse");
 
-
-            PointerInfo a = MouseInfo.getPointerInfo();
-            Point b = a.getLocation();
-            int x = (int) b.getX();
-            int y = (int) b.getY();
-
-            Duration timeElapsed = Duration.between(last_time, start);
-            if (timeElapsed.toMillis() > ONE_Minute) {
+                // Move the mouse
                 robot.mouseMove(random.nextInt(MAX_X), random.nextInt(MAX_Y));
             }
 
-            if (last_x != x) {
-                last_time = Instant.now();
-            }
-
+            // Set last mouse position
             last_x = x;
+            last_y = y;
 
+            // Wait one minute
             try {
-                Thread.sleep(1000);
+                Thread.sleep(One_Minute);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
